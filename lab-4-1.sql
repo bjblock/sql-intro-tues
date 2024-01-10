ba61f24dc6d58b3eaa3de8bc7a640d957d29edd7
Benjamin Block <bjblock@gmail.com>
2024-01-10 17:35:02 +0000

    lab 3 and barry bonds hits

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -1,5 +1,11 @@
 -- How many lifetime hits does Barry Bonds have?
 
+SELECT SUM(stats.hits)
+FROM stats
+INNER JOIN players ON stats.player_id = players.id
+WHERE first_name = "Barry"
+AND last_name = "Bonds";
+
 -- Expected result:
 -- 2935
 
